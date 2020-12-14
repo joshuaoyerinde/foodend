786cd4fc88acc6dc8bb3a2865d1c1694fbcb516c
joshuaoyerinde <oyerindejoshua133@gamil.com>
2020-12-14 11:53:54 +0100

    done

--- db.php
+++ db.php
@@ -1,5 +1,6 @@
 <?php 
-header("Access-Control-Allow-Origin: *");
+// session_start();
+header("Access-Control-Allow-Origin: http://localhost:8081");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Origin, X-Reguested-With, Content-Type, Accept");
 header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
@@ -8,6 +9,7 @@ header("Access-Control-Max-age: 86400");
 	$connect=mysqli_connect('localhost','root','','foodapp_bd');
 	if ($connect) {
 		// echo "success";
+		// echo $_SESSION['user_id']; 
 	}else{
 		// echo "never";
 	}
